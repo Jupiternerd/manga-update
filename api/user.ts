091b4a09e066be0effa3b01626dd60c3300828e2
Shokkunn <mincl5792@gmail.com>
2024-05-17 16:27:40 -0400

    idk

--- api/user.ts
+++ api/user.ts
@@ -5,6 +5,7 @@ import pg from "pg";
 export default async function handler(req: VercelRequest, res: VercelResponse) {
     const client = new pg.Client(process.env.POSTGRES_URL)
     await client.connect()
+    console.log(req)
     try {
         const { rows } = await client.query('SELECT * FROM users')
         res.status(200).json(rows)
